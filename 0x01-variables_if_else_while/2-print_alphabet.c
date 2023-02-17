cb470fb5a08d801bd06cb17d062c8457d3d8c2b9
Martinson Joshua Tetteh <hosuatetteh@gmail.com>
2023-02-17 09:03:49 -0800

    Alphabets -i

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry Point of program
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char alpha_low[] = "abcdefghijklmnopqrstuvwxyz";
+	unsigned long int i = 0;
+
+	for (; i < sizeof(alpha_low); i++)
+	{
+		putchar(alpha_low[i]);
+	}
+	putchar('\n');
+	return (0);
+}
