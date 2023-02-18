cf9718740c8a75c3fdb6529421c0996bcff1e9cc
Martinson Joshua Tetteh <hosuatetteh@gmail.com>
2023-02-18 02:38:10 -0800

    Print Tebahpla -i

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char low_alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = sizeof(low_alpha) - 1;
+
+	for (; i >= 0; i--)
+		putchar(low_alpha[i]);
+	putchar('\n');
+	return (0);
+}
