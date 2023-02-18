0336eab160b60e8ea6d82f5f8eabd459aa0e8e4c
Martinson Joshua Tetteh <hosuatetteh@gmail.com>
2023-02-18 02:21:53 -0800

    Print Numbers -i

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+		printf("%i", i);
+	putchar('\n');
+	return (0);
+}
