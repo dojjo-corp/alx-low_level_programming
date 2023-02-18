5a595c9fb657d9f29c4c06a0a1d1774ca407f12a
Martinson Joshua Tetteh <hosuatetteh@gmail.com>
2023-02-18 02:30:38 -0800

    Print Numberz -i

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int n = 48;
+
+	for (; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
