866943e0fda5d726082e0eecf8f7ab6141508113
Martinson Joshua Tetteh <hosuatetteh@gmail.com>
2023-02-22 01:36:29 -0800

    Alphabet -i

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,3 +15,20 @@ void print_char(void)
 	}
 	putchar('\n');
 }
+
+/**
+ * print_alphabet - Print lowercase alphabets
+ *
+ */
+
+void print_alphabet(void)
+{
+	char low_apha[] = "abcdefghijklmnopqrstuvwxyz";
+	unsigned long int i = 0;
+
+	for (; i < sizeof(low_alpha) - 1; i++)
+	{
+		putchar(low_alpha[i]);
+	}
+	putchar('\n');
+}
