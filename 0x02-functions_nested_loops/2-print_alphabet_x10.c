92ba55db53bbe88672f338c2acbe19df995c20f9
Martinson Joshua Tetteh <hosuatetteh@gmail.com>
2023-02-22 02:33:47 -0800

    Print Lphabet x10 -i

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/** print_alphabet_x10 - Print lowercase aphabets 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char low_alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	unsigned long int i = 0;
+	int j = 0;
+
+	for (; j < 10; j++)
+	{
+		for (; i <sizeof(low_alpha) - 1; i++)
+		{
+			_putchar(low_alpha[i]);
+		}
+		_putchar('\n');
+	}
+}
