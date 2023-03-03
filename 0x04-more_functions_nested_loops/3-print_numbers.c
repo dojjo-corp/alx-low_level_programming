9ccbf136f28538830c2dd6acb6553dfd6f6964ca
Martinson Joshua Tetteh <hosuatetteh@gmail.com>
2023-03-03 12:38:46 -0800

    Print Numbers -i

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * print_numbers - Print all single digit integers
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58 i++)
+		_putchar(i);
+	_putchar('\n');
+}
