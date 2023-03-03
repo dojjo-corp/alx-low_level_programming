8a31167a6b51280fb734562f11ef9f3771aa7b9a
Martinson Joshua Tetteh <hosuatetteh@gmail.com>
2023-03-02 18:02:12 -0800

    IsUpper -i

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if char is an uppercase letter
+ * @c: Character to be checked
+ * Return: Always 0
+ */
+
+int _isupper(int c)
+{
+	if isupper(c)
+		return (1);
+	else
+		return (0);
+}
